e43a4d520369304a2e8b5019e1dfae20bb6bcde8
Ruslan <ruslas@yandex.ru>
2024-10-03 00:29:50 +0300

    final version

--- spec/fetchUsers.spec.js
+++ spec/fetchUsers.spec.js
@@ -6,7 +6,7 @@ describe('Тестирование асинхронной функции fetchUs
   let result;
   it('Проверка результата при статусе 200', async () => {
     result = await fetchUsers();
-    expect(await result).to.equal(
+    expect(result).to.equal(
       'Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, Chelsey Dietrich, Mrs. Dennis Schulist, Kurtis Weissnat, Nicholas Runolfsdottir V, Glenna Reichert, Clementina DuBuque'
     );
   });
